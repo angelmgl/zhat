492da9127aadf5e6b5b7bc43ca4ca84d5508e47e
Angel Mgl <angelemegeele@gmail.com>
2021-02-23 01:40:19 -0300

    ChatTitle component modified

--- src/components/ChatUi/ChatFeed/TitleSection/index.js
+++ src/components/ChatUi/ChatFeed/TitleSection/index.js
@@ -10,45 +10,17 @@ export default class Title extends Component {
         if (!chat) { return <div /> }
 
         return (
-            <div 
-                className='ce-chat-title'
-            >
-                <div style={styles.titleContainer} className='ce-chat-title-container'>  
-                    <div style={styles.titleText} className='ce-chat-title-text'>
-                        { chat && chat.title }
-                    </div>
-                    
-                    <div style={styles.subtitleText} className='ce-chat-subtitle-text'>
+            <div className="chat-title-container">
+                <div className="chat-title">
+                    <h2>{ chat && chat.title }</h2>
+                    <p>
                         {
                             chat.last_message.created && chat.last_message.created.length > 0 &&
                             `Activo el ${timeSinceDate(chat.last_message.created)}`
                         }
-                    </div>
+                    </p>
                 </div>
             </div>
         );
-    }
-}
-
-const styles = {
-    mobileSection: {
-        textAlign: 'center', 
-        padding: '30px 0px', 
-        backgroundColor: 'rgb(256, 256, 256, 0.92)',
-        border: '1px solid rgb(256, 256, 256, 0.92)'
-    },
-    titleContainer: {
-        width: '100%',
-        padding: '18px 0px',
-        textAlign: 'center',
-        color: 'rgb(24, 144, 255)',
-        backgroundColor: 'rgb(256, 256, 256, 0.92)'
-    },
-    titleText: {
-        fontSize: '24px',
-        fontWeight: '600',
-    },
-    subtitleText: {
-        fontSize: '12px',
     }
 }
